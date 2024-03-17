7169036897fc5d04e06f07b8cc0f0e5785a0cd0a
Elis Antonio Perez <codewithleader@gmail.com>
2024-03-17 14:04:08 -0500

    feat: :sparkles: print() function
    Learning Python. Print function

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python - Bases
+
+@codewithleader
---
+++ print.py
@@ -0,0 +1,6 @@
+# Imprime "Your name is " + el nombre que escriba el user + " " + el apellido que escriba el user
+# print("Your name is " + input("What's your name? ") + " " + input("What's your lastname? "))
+
+name = input("What's your name? ")
+lastName = input("What's your lastname? ")
+print("Your name is " + name + " " + lastName)
