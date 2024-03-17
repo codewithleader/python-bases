7bb13c71e80737e2d68f25d5c7d2cddfc1276d2b
Elis Antonio Perez <codewithleader@gmail.com>
2024-03-17 14:33:13 -0500

    feat: :tada: Print + Input

--- print.py
+++ print.py
@@ -1,6 +1,10 @@
 # Imprime "Your name is " + el nombre que escriba el user + " " + el apellido que escriba el user
 # print("Your name is " + input("What's your name? ") + " " + input("What's your lastname? "))
 
-name = input("What's your name? ")
-lastName = input("What's your lastname? ")
-print("Your name is " + name + " " + lastName)
+# name = input("What's your name? ")
+# lastName = input("What's your lastname? ")
+# print("Your name is " + name + " " + lastName)
+
+brandName = input("What's the name for your brand? ")
+slogan = input("What's the slogan? ")
+print("\"" + brandName + "\"" "\n" + slogan)
